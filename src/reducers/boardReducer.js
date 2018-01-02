f0cfefefbd9c99b0d7fe26ae2b3f13a087e56bdf
Kutay Kocayurt <kkocayurt@babbel.com>
2018-01-03 00:26:08 +0100

    update board on toggle click

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,23 +1,31 @@
 import { createBoard, tick } from '../lib/game';
 
-export default (state = {
-    board: createBoard(20, 20, true)
-  }, action) => {
-    switch (action.type) {
-      case 'GET_RANDOM_BOARD': {
-        return {
-          ...state,
-          board: createBoard(20, 20, true)
-        }
-      }
-      case 'NEXT': {
-        return {
-          ...state,
-          board: tick(state.board)
-        }
-      }
-      default: {
-        return state;
+export default (state = { board: createBoard(20, 20) }, action) => {
+  switch (action.type) {
+    case 'GET_RANDOM_BOARD': {
+      return {
+        ...state,
+        board: createBoard(20, 20, true)
       }
     }
+    case 'TOGGLE_CELL': {
+      const newBoard = [...state.board];
+      const cellValue = state.board[action.row][action.col] ? 0 : 1;
+      newBoard[action.row][action.col] = cellValue;
+
+      return {
+        ...state,
+        board: newBoard
+      }
+    }
+    case 'NEXT': {
+      return {
+        ...state,
+        board: tick(state.board)
+      }
+    }
+    default: {
+      return state;
+    }
+  }
 }
